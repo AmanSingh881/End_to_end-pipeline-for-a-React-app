5485afb8ae143a9d8fe517247978b159d319488d
Aman Singh <102499131+AmanSingh881@users.noreply.github.com>
2024-01-22 15:50:42 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home00
+                Home
               </Link>
             </li>
             <li className="nav-item">
